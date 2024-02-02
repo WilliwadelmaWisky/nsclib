cf9db1fdc902070815d34b01f6823abf53a54101
JS <minecraftprofile52@gmail.com>
2024-02-02 12:44:46 +0200

    Added standard uncertainty propagation method

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,8 @@ pub mod constant;
 
 fn main() {
     let point = vector::new(&[1.0, 2.0, 3.0]);
-
-    let res1 = differentiate::partial_df(|x| x[0].powi(2) * x[1] * x[2].powi(4), &point, 0, 0.001);
-    println!("{res1}");
-
-    let res2 = differentiate::partial_df(|x| x[0].powi(2) * x[1] * x[2].powi(4), &point, 1, 0.001);
-    println!("{res2}");
-
-    let res3 = differentiate::partial_df(|x| x[0].powi(2) * x[1] * x[2].powi(4), &point, 2, 0.001);
-    println!("{res3}");
+    let err = vector::new(&[0.01, 0.2, 0.08]);
+    let val = point[0].powi(2) * point[1].powi(3) * point[2].powi(5);
+    let unc = uncertainty::calc_standard(|x| x[0].powi(2) * x[1].powi(3) * x[2].powi(5), &point, &err);
+    println!("Uncertainty: {val}+-{unc}");
 }
---
+++ src/uncertainty.rs
@@ -0,0 +1,35 @@
+use crate::differentiate;
+
+
+/// Calculates an uncertainty using a standard uncertainty propagation method
+/// 
+/// # Arguments
+/// - `f`: Function to calculate a variable
+/// - `point`: Point where the uncertainty is calculated
+/// - `err`: List of uncertainties of variables in f
+/// 
+/// # Examples
+/// 
+pub fn calc_standard(f: fn(&Vec<f64>) -> f64, point: &Vec<f64>, err: &Vec<f64>) -> f64 {
+    let mut uncertainty = 0.0;
+    let h = 0.00001;
+
+    for i in 0..err.len() {
+        let df = differentiate::partial_df(f, point, i, h);
+        uncertainty += (df * err[i]).powi(2);
+    }
+
+    return uncertainty.sqrt();
+}
+
+/// Calculates an uncertainty using a minmax uncertainty propagation method
+/// 
+/// # Arguments
+/// - `f`: Function to calculate a variable
+/// - `point`: Point where the uncertainty is calculated
+/// - `err`: List of uncertainties of variables in f
+/// 
+/// # Examples
+pub fn calc_minmax(f: fn(&Vec<f64>) -> f64, point: &Vec<f64>, err: &Vec<f64>) -> f64 {
+    return 0.0;
+}
